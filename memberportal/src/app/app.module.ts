5919513c7a6527f80cd7579d1be6d440050f72e3
snehase25 <snehajobs25@gmail.com>
2021-10-07 13:16:36 +0530

    Added Member components

--- memberportal/src/app/app.module.ts
+++ memberportal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './Member/list/list.component';
+import { EditComponent } from './Member/edit/edit.component';
+import { AddComponent } from './Member/add/add.component';
+import { DetailComponent } from './Member/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    AddComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
